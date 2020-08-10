ad192416e41d67940ffb444807b42f67fa555e1d
Gordon Caister <gordoncaister@gmail.com>
2020-08-10 17:45:12 -0500

    Updated index.js to render landing page, added landing page

---
+++ src/components/pages/Landing/Landing.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Card } from 'antd';
+import { Button } from 'antd';
+
+const Landing = () => {
+  return (
+    <>
+      <Card title="Requires your consent" style={{ width: 300 }}>
+        <Button type="primary">Take me away</Button>
+        <></>
+        <></>
+        <Button type="primary">Let me see!</Button>
+      </Card>
+    </>
+  );
+};
+export default Landing;
