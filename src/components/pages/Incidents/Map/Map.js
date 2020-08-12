b6d9d7946a33aabaee15a3fa842ab09dd6cd7f3b
Nathan <nateyl.b@gmail.com>
2020-08-12 14:49:50 -0700

    google map is now displaying

--- src/components/pages/Incidents/Map/Map.js
+++ src/components/pages/Incidents/Map/Map.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import GoogleMapReact from 'google-map-react';
+import Marker from './Marker';
 
 import { axiosBase } from '../../../../utils/axiosBase';
 
 const Map = props => {
   const [incidents, setIncidents] = useState([]);
+  const center = {
+    lat: 59.95,
+    lng: 30.33,
+  };
+  const zoom = 11;
 
   useEffect(() => {
     axiosBase()
@@ -17,7 +24,17 @@ const Map = props => {
       });
   }, []);
   console.log(incidents, 'STATE');
-  return <div></div>;
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        <Marker lat={59.955413} lng={30.337844} text="My Marker" />
+      </GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
--- src/components/pages/Incidents/Map/Marker.js
+++ src/components/pages/Incidents/Map/Marker.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Marker = props => {
-  return <div className="marker-container"></div>;
+  return <div className="marker-container">HELLO</div>;
 };
 
 export default Marker;
