45e0fa2df5861535d9b216a545e9300295da8284
Cole Wilkison <wilkison.cole@gmail.com>
2020-08-13 17:49:23 -0400

    added hover functionality for incidents on map, allowing acces to future data

--- src/components/pages/Incidents/Map/Marker.js
+++ src/components/pages/Incidents/Map/Marker.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Popover } from 'antd';
 
 const Marker = props => {
-  return <div className="marker"></div>;
+  const [click, setClick] = useState(false);
+  const [hover, setHover] = useState(false);
+
+  const handleHide = () => (setClick(false), setHover(false));
+
+  const handleHover = visible => (setHover(visible), setClick(false));
+
+  const handleClick = visible => (setHover(false), setClick(visible));
+
+  return (
+    <>
+      <Popover
+        style={{ width: '500px' }}
+        content="Hello"
+        title="Hi There"
+        trigger="hover"
+        visible={hover}
+        onVisibleChange={handleHover}
+      >
+        <Popover
+          style={{ width: '500px' }}
+          content="Hello"
+          title="Hi There"
+          trigger="click"
+          visible={click}
+          onVisibleChange={handleClick}
+        >
+          <div
+            className="marker"
+            style={{ width: '25px', height: '25px', backgroundColor: 'red' }}
+          ></div>
+          <a onClick={handleHide}>Close</a>
+        </Popover>
+      </Popover>
+    </>
+  );
 };
 
 export default Marker;
