2c97e29d56d4113b50c58854a0124a17205401ce
Nathan <nateyl.b@gmail.com>
2020-09-16 11:12:08 -0700

    formatted dates to match dates on the rest of the site

--- src/components/pages/Incidents/Timeline/LocalSlider.js
+++ src/components/pages/Incidents/Timeline/LocalSlider.js
@@ -26,7 +26,11 @@ const LocalSlider = props => {
   );
 
   function formatter(value) {
-    return `${dateArray[value]}`;
+    return new Date(dateArray[value]).toLocaleDateString('en-us', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    });
   }
 
   const onSlide = value => {
@@ -39,20 +43,30 @@ const LocalSlider = props => {
       <div className="date-range-container">
         <div>
           {props.startDate
-            ? props.startDate
-            : `${startDate.getFullYear()}-${(
-                '0' +
-                (startDate.getMonth() + 1)
-              ).slice(-2)}-${startDate.getDate()}`}
+            ? new Date(props.startDate).toLocaleDateString('en-us', {
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric',
+              })
+            : new Date(startDate).toLocaleDateString('en-us', {
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric',
+              })}
         </div>
         <div> - </div>
         <div>
           {props.endDate
-            ? props.endDate
-            : `${endDate.getFullYear()}-${(
-                '0' +
-                (endDate.getMonth() + 1)
-              ).slice(-2)}-${endDate.getDate()}`}
+            ? new Date(props.endDate).toLocaleDateString('en-us', {
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric',
+              })
+            : new Date(endDate).toLocaleDateString('en-us', {
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric',
+              })}
         </div>
       </div>
       <Slider
