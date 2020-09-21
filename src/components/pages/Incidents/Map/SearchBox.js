99c290622874da3a438162e93d4c1e4172708530
Gordon Caister <gordoncaister@gmail.com>
2020-09-21 12:17:26 -0500

    Conversion: converted autocomplete.js and searchbox.js from class components to functional components for consistency in codebase

--- src/components/pages/Incidents/Map/SearchBox.js
+++ src/components/pages/Incidents/Map/SearchBox.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Form, Row, Col } from 'antd';
 import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
-import { AutoComplete } from './Autocomplete';
-class SearchBox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      address: '',
-    };
-  }
+import AutoComplete from './Autocomplete';
+const SearchBox = props => {
+  const [address, setAddress] = useState('');
 
-  onPlaceChanged = address => {
-    console.log(address);
+  const onPlaceChanged = address => {
     geocodeByAddress(address)
       .then(async results => {
         if (!results[0].geometry) return;
         if (results[0].geometry.viewport) {
-          this.props.map.fitBounds(results[0].geometry.viewport);
+          props.map.fitBounds(results[0].geometry.viewport);
         } else {
-          this.props.map.setCenter(results[0].geometry.location);
-          this.props.map.setZoom(17);
+          props.map.setCenter(results[0].geometry.location);
+          props.map.setZoom(17);
         }
         return getLatLng(results[0]);
       })
@@ -28,26 +22,23 @@ class SearchBox extends Component {
       });
   };
 
-  clearAddress() {
-    this.setState({ address: '' });
-  }
+  const clearAddress = () => {
+    setAddress('');
+  };
 
-  render() {
-    return (
-      <Form className="autocomplete-search">
-        <Row>
-          <Col>
-            <AutoComplete
-              address={this.state.address}
-              clearAddress={this.clearAddress}
-              onChange={this.handleAddressChange}
-              onAddressSelect={this.onPlaceChanged}
-            />
-          </Col>
-        </Row>
-      </Form>
-    );
-  }
-}
+  return (
+    <Form className="autocomplete-search">
+      <Row>
+        <Col>
+          <AutoComplete
+            address={address}
+            clearAddress={clearAddress}
+            onAddressSelect={onPlaceChanged}
+          />
+        </Col>
+      </Row>
+    </Form>
+  );
+};
 
 export default SearchBox;
